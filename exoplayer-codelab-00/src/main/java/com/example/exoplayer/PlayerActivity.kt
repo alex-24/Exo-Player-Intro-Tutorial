f528c9b352e9c0936c388de2732ee7b68ce67dbb
Alexis CASSION <acassion@dotscreen.com>
2024-04-30 11:06:37 +0200

    00

--- exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
+++ exoplayer-codelab-00/src/main/java/com/example/exoplayer/PlayerActivity.kt
@@ -17,6 +17,12 @@ package com.example.exoplayer
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.media3.common.MediaItem
+import androidx.media3.common.util.Util
+import androidx.media3.exoplayer.ExoPlayer
 import com.example.exoplayer.databinding.ActivityPlayerBinding
 
 /**
@@ -24,12 +30,79 @@ import com.example.exoplayer.databinding.ActivityPlayerBinding
  */
 class PlayerActivity : AppCompatActivity() {
 
-    private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
-        ActivityPlayerBinding.inflate(layoutInflater)
+    private var player: ExoPlayer? = null
+
+    private var playWhenReady = true
+    private var mediaItemIndex = 0
+    private var playbackPosition = 0L
+
+    private val bindings by lazy(LazyThreadSafetyMode.NONE) {
+        ActivityPlayerBinding.inflate(this.layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(viewBinding.root)
+        setContentView(this.bindings.root)
+    }
+
+    private fun initializePlayer() {
+        this.player = ExoPlayer.Builder(this)
+            .build()
+            .also { exoPlayer ->
+                this.bindings.videoView.player = exoPlayer
+                val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
+                //exoPlayer.setMediaItem(mediaItem)
+                exoPlayer.setMediaItems(listOf(mediaItem), this.mediaItemIndex, this.playbackPosition)
+                exoPlayer.playWhenReady = this.playWhenReady
+                exoPlayer.prepare()
+            }
+    }
+
+    private fun releasePlayer() {
+        this.player?.let { exoPlayer ->
+            this.playbackPosition = exoPlayer.currentPosition
+            this.mediaItemIndex = exoPlayer.currentMediaItemIndex
+            this.playWhenReady = exoPlayer.playWhenReady
+            exoPlayer.release()
+        }
+        this.player = null
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) { // multi window supported
+            initializePlayer()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if (Util.SDK_INT <= 23) {
+            initializePlayer()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT > 23) { // multi window supported
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT <= 23) {
+            releasePlayer()
+        }
+    }
+
+    private fun hideSystemUi() {
+        WindowCompat.setDecorFitsSystemWindows(this.window, false)
+        WindowInsetsControllerCompat(window, this.bindings.videoView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
     }
 }
